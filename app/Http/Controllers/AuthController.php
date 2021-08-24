540272527bdb63f6b8efbc3af5dcdd73623fd16b
HiuberMorente <hiuberr@gmail.com>
2021-08-23 23:45:19 -0600

    login fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
     public function login(Request $request){
         if(!Auth::attempt($request->only('email', 'password'))){
             return response([
-                'message' =>'Invalid credentials!'
+                'message' =>'Invalid'
             ], Response::HTTP_UNAUTHORIZED);
         }
 
