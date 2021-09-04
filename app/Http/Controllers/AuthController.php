1a2e65e4c955339b9c7c5bad16c487d680fc6ac2
HiuberMorente <hiuberr@gmail.com>
2021-09-04 13:51:36 -0600

    fixing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
         $user = Auth::user();
 
-        $token= $user->createToken('token')->plainTextToken;
+        $token= $request -> $user->createToken('token')->plainTextToken;
         
         $cookie = cookie('jwt', $token, 60 * 24); //1 day
 
