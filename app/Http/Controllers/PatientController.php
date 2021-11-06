7425c7107ca538b497f29acf893b344b86873419
HiuberMorente <hiuberr@gmail.com>
2021-11-05 23:56:45 -0600

    delete

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -35,8 +35,6 @@ class PatientController extends Controller
         return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
-    public function dude(Request $request){
-        return $request;
-    }
+    
 
 }
